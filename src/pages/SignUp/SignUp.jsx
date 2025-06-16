faff101c1d800951d6f9cc5bdd5ffec826869636
Jahirul83 <jahirulislamcse83@gmail.com>
2025-06-16 19:13:19 +0600

    signup with google successful

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -31,13 +31,10 @@ const SignUp = () => {
       const { data } = await axios.post(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`, formData)
       console.log(data.data.display_url)
 
-
-
       // create user
 
       const result = await createUser(email, password)
       console.log(result)
-
 
       // save user name and photo in firebase
 
@@ -53,6 +50,31 @@ const SignUp = () => {
       toast.error(error.message)
       setLoading(false)
     }
+
+
+
+  }
+
+  // handle google signin
+
+  const handleGoogleSignin = async () => {
+
+    try {
+      await signInWithGoogle()
+      navigate('/')
+
+      toast.success('signup successfully')
+
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+      setLoading(false)
+    }
+
+
+
+
   }
   return (
     <div className='flex justify-center items-center min-h-screen'>
@@ -129,7 +151,7 @@ const SignUp = () => {
               type='submit'
               className='bg-rose-500 w-full rounded-md py-3 text-white'
             >
-              {loading ? <TbFidgetSpinner className='animate-spin m-auto'/> : "Continue"}
+              {loading ? <TbFidgetSpinner className='animate-spin m-auto' /> : "Continue"}
             </button>
           </div>
         </form>
@@ -140,11 +162,11 @@ const SignUp = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button onClick={handleGoogleSignin} disabled={loading} className='disabled:cursor-not-allowed cursor-pointer flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
+        </button>
         <p className='px-6 text-sm text-center text-gray-400'>
           Already have an account?{' '}
           <Link
